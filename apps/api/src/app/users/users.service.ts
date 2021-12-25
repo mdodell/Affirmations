04c1a7cc856b66bc193bfde68e1b2c1cad222362
mdodell <dodellmitchell@gmail.com>
2021-12-25 02:49:14 -0500

    Removing cron job for now that I know it works

--- apps/api/src/app/users/users.service.ts
+++ apps/api/src/app/users/users.service.ts
@@ -3,17 +3,11 @@ import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
 
 import * as bcrypt from 'bcrypt';
-import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class UsersService {
   private readonly logger = new Logger(UsersService.name);
   constructor(@InjectModel(User) private userModel: typeof User) {}
-
-  @Cron('* * * * * *')
-  handleCron() {
-    this.logger.debug('Called when the current second is 1');
-  }
 
   async create(user: User): Promise<User | void> {
     const { password, ...rest } = user;
